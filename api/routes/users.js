3cfb65b36474e1e76835d103d132980647068388
rahulramey <rahulramey562@gmail.com>
2024-07-02 22:05:18 +0530

    follow, unfollow api

--- api/routes/users.js
+++ api/routes/users.js
@@ -55,3 +55,49 @@ router.get("/:id", async(req,res)=>{
         return res.status(404).json("No user found!");
     }   
 });
+
+//follow a user
+router.put("/:id/follow", async(req,res)=>{
+    if(req.body.userId !== req.params.id){
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+           
+            if(!user.followers.includes(req.body.userId)){
+                await user.updateOne({$push: {followers: req.body.userId }});
+                await currentUser.updateOne({$push: {followings: req.params.id }});
+                return res.status(200).json("You started following this account!");
+            }else{
+                return res.status(403).json("You already follow this account!")
+            }
+            
+        } catch (error) {
+            return res.status(404).json(error);  
+        }
+    }else{
+        return res.status(403).json("You cannot follow yourself!")
+    }
+})
+
+//unfollow a user
+router.put("/:id/unfollow", async(req,res)=>{
+    if(req.body.userId !== req.params.id){
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+           
+            if(user.followers.includes(req.body.userId)){
+                await user.updateOne({$pull: {followers: req.body.userId }});
+                await currentUser.updateOne({$pull: {followings: req.params.id }});
+                return res.status(200).json("You unfollowed this account!");
+            }else{
+                return res.status(403).json("You dont follow this account!")
+            }
+            
+        } catch (error) {
+            return res.status(404).json(error);  
+        }
+    }else{
+        return res.status(403).json("You cannot unfollow yourself!")
+    }
+})
